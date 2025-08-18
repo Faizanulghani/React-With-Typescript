e70030426408df85c7daf5a05fbe026d362acaf4
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-08-18 18:11:45 +0500

    Added Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import { useState } from "react";
 import MyButton from "./components/MyButton";
 
-interface Book {
-  name: string;
-  price: number;
-}
-
 function App() {
-  const [value, setValue] = useState<Book>({
-    name: "One",
-    price: 10,
-  });
+
   return (
     <>
-      <h3>
-        Name: {value.name} (Rs. {value.price}){" "}
-      </h3>
       <MyButton
-        onClick={() => setValue({ name: "Two", price: 20 })}
         text="MyButton"
       />
     </>
--- src/components/MyButton.tsx
+++ src/components/MyButton.tsx
@@ -1,12 +1,31 @@
 import type React from "react";
+import { useState } from "react";
 
 interface MyButtonProps {
   text: string;
   onClick?: () => void;
 }
 
+interface Book {
+  name: string;
+  price: number;
+}
+
 const MyButton: React.FC<MyButtonProps> = (props) => {
-  return <button onClick={props.onClick}>{props.text}</button>;
+  const [value, setValue] = useState<Book>({
+    name: "One",
+    price: 10,
+  });
+  return (
+    <>
+      <h3>
+        Name: {value.name} (Rs. {value.price})
+      </h3>
+      <button onClick={() => setValue({ name: "Two", price: 20 })}>
+        {props.text}
+      </button>
+    </>
+  );
 };
 
 export default MyButton;
