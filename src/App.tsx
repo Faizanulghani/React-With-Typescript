33253ad9519c8e3df8c6d22b74fb3ae41e551000
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-08-18 17:30:10 +0500

    Added button using type script

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-function App() {
+import MyButton from "./components/MyButton";
 
+function App() {
   return (
     <>
-      
+      <MyButton onClick={() => alert("clicked")} text="MyButton" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
